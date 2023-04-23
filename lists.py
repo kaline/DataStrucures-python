a68367612b8801c7db62484e8d3e046da3e8c9b7
kaline <mesquitabfkaline@gmail.com>
2023-04-23 12:17:46 -0300

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,14 @@
+lista_simples_inteiro = [1, 2, 3, 8, 14, 4, 5]
+listas_simples_string = ["Olá", "Mundo"]
+lista_simples_mesclada = [1, 2, 3, "Olá, mundo", True, 1.5]
+
+print(lista_simples_inteiro)
+print(listas_simples_string)
+print(lista_simples_mesclada)
+
+for i in range(3,len(lista_simples_inteiro), 2):
+    print(i)
+    break;
+
+
+
